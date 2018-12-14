49220b9efad596aee51cb84911530890d1b91202
Igi <brad@igi.codes>
2018-12-14 22:55:20 +0000

    Updated nuget packages

--- Discord.Client/Properties/AssemblyInfo.cs
+++ Discord.Client/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using NFive.SDK.Client;
 
-[assembly: ClientPlugin]
+[assembly: ClientPlugin(1)]
 
 [assembly: AssemblyTitle("NFive Discord")]
 [assembly: AssemblyDescription("Plugin to set client's Discord rich presence")]
--- Discord.Server/Properties/AssemblyInfo.cs
+++ Discord.Server/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using NFive.SDK.Server;
 
-[assembly: ServerPlugin]
+[assembly: ServerPlugin(1)]
 
 [assembly: AssemblyTitle("NFive Discord")]
 [assembly: AssemblyDescription("Plugin to set client's Discord rich presence")]

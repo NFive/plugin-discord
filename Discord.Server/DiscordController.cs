9b246a9260327b704a6b1f551029e8c8e6705945
Joe Biellik <joebiellik@gmail.com>
2018-12-28 21:11:13 +0000

    Update SDK

--- Discord.Server/DiscordController.cs
+++ Discord.Server/DiscordController.cs
@@ -3,6 +3,7 @@ using NFive.Discord.Shared;
 using NFive.SDK.Core.Diagnostics;
 using NFive.SDK.Server.Controllers;
 using NFive.SDK.Server.Events;
+using NFive.SDK.Server.Rcon;
 using NFive.SDK.Server.Rpc;
 
 namespace NFive.Discord.Server
@@ -10,7 +11,7 @@ namespace NFive.Discord.Server
 	[PublicAPI]
 	public class DiscordController : ConfigurableController<Configuration>
 	{
-		public DiscordController(ILogger logger, IEventManager events, IRpcHandler rpc, Configuration configuration) : base(logger, events, rpc, configuration)
+		public DiscordController(ILogger logger, IEventManager events, IRpcHandler rpc, IRconManager rcon, Configuration configuration) : base(logger, events, rpc, rcon, configuration)
 		{
 			this.Rpc.Event(DiscordEvents.GetConfig).On(e => e.Reply(this.Configuration));
 		}
